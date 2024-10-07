c2a35582dcc3c67ffe01492718f7780f7af5fdc7
Vinícius Fernandes <business.fernandes22@gmail.com>
2024-10-07 19:53:44 -0300

    task 4

---
+++ 4-cria-tabela-car-brand.sql
@@ -0,0 +1,14 @@
+CREATE TABLE CAR_BRAND (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    BRAND_NAME VARCHAR(32) NOT NULL
+);
+
+INSERT INTO CAR_BRAND (BRAND_NAME) VALUES
+('Chevrolet'),
+('Toyota'),
+('Hyundai'),
+('Volkswagen'),
+('Jeep'),
+('Renault'),
+('Honda'),
+('Fiat');
